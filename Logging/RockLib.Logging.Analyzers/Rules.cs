582a8084923a343c786dd2614c090ab965c8ba28
Brian Friesen <brianfriesen@quickenloans.com>
2021-06-01 16:25:09 -0400

    Add rule RockLib0000 and docs stub

--- Logging/RockLib.Logging.Analyzers/Rules.cs
+++ Logging/RockLib.Logging.Analyzers/Rules.cs
@@ -6,6 +6,13 @@ namespace RockLib.Logging.Analyzers
     {
         public const string HelpLinkFormat = "https://github.com/RockLib/RockLib.Analyzers/blob/master/Rules/{0}.md";
 
+        public static DiagnosticDescriptor RockLib0000 { get; } = GetRule(
+            nameof(RockLib0000),
+            "Extended property is not marked as safe to log",
+            "The value of a sanitized extended property should have a type with one or more properties decorated with the [SafeToLog] or else be decorated with the [SafeToLog] attribute itself.",
+            DiagnosticCategory.Usage,
+            DiagnosticSeverity.Warning);
+
         private static DiagnosticDescriptor GetRule(string id, string title, string messageFormat,
             DiagnosticCategory category, DiagnosticSeverity severity) =>
             new DiagnosticDescriptor(id, title, messageFormat, category.ToString(), severity,
---
+++ Rules/RockLib0000.md
@@ -0,0 +1,27 @@
+# RockLib0000: Extended property is not marked as safe to log
+
+## Cause
+
+
+
+## Reason for rule
+
+
+
+## How to fix violations
+
+
+
+## Examples
+
+### Violates
+
+
+
+### Does not violate
+
+
+
+## How to suppress violations
+
+
