805d54514705f4a748c071ab41cc6f289957cb03
Brian Friesen <brianfriesen@quickenloans.com>
2021-06-09 15:00:14 -0400

    Fix help link uri

--- Logging/RockLib.Logging.Analyzers/HelpLinkUri.cs
+++ Logging/RockLib.Logging.Analyzers/HelpLinkUri.cs
@@ -2,6 +2,6 @@
 {
     internal static class HelpLinkUri
     {
-        public const string Format = "https://github.com/RockLib/RockLib.Analyzers/blob/master/Rules/{0}.md";
+        public const string Format = "https://github.com/RockLib/RockLib.Analyzers/blob/main/Rules/{0}.md";
     }
 }
