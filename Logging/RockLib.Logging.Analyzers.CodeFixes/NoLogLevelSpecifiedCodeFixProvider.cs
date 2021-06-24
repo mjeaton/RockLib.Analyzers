8be6a6aa74853c542ec1458653d772412cab606f
Brian Friesen <brianfriesen@quickenloans.com>
2021-06-24 19:47:03 -0400

    Add analyzer and codefix for RockLib0005

---
+++ Logging/RockLib.Logging.Analyzers.CodeFixes/NoLogLevelSpecifiedCodeFixProvider.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
+using RockLib.Analyzers.Common;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RockLib.Logging.Analyzers
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(NoLogLevelSpecifiedCodeFixProvider)), Shared]
+    public class NoLogLevelSpecifiedCodeFixProvider : CodeFixProvider
+    {
+        public const string SetLevelPropertyTo = "Set LogEntry.Level to ";
+        public const string SetLevelParameterTo = "Set LogEntry.Level to ";
+
+        private static readonly string[] _levels = { "Debug", "Info", "Warn", "Error", "Fatal", "Audit" };
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(DiagnosticIds.NoLogLevelSpecified);
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken);
+            var semanticModel = await context.Document.GetSemanticModelAsync(context.CancellationToken);
+
+            foreach (var diagnostic in context.Diagnostics)
+            {
+                var node = root.FindNode(diagnostic.Location.SourceSpan);
+                var logEntryArgument = semanticModel.GetOperation(node) as IArgumentOperation;
+                var logEntryCreation = GetLogEntryCreationOperation(logEntryArgument);
+
+                if (logEntryCreation.Arguments.Length > 0)
+                {
+                    foreach (var level in _levels)
+                    {
+                        context.RegisterCodeFix(
+                            CodeAction.Create(
+                                title: SetLevelParameterTo + level,
+                                createChangedDocument: cancellationToken =>
+                                    SetLevelParameter(context.Document, logEntryCreation, level, cancellationToken),
+                                equivalenceKey: nameof(SetLevelParameterTo) + level),
+                            diagnostic);
+                    }
+                }
+                else
+                {
+                    foreach (var level in _levels)
+                    {
+                        context.RegisterCodeFix(
+                            CodeAction.Create(
+                                title: SetLevelPropertyTo + level,
+                                createChangedDocument: cancellationToken =>
+                                    SetLevelProperty(context.Document, logEntryCreation, level, cancellationToken),
+                                equivalenceKey: nameof(SetLevelPropertyTo) + level),
+                            diagnostic);
+                    }
+                }
+            }
+        }
+
+        private static async Task<Document> SetLevelProperty(
+            Document document,
+            IObjectCreationOperation logEntryCreationOperation,
+            string logLevel,
+            CancellationToken cancellationToken)
+        {
+            var logEntryCreationExpression = (BaseObjectCreationExpressionSyntax)logEntryCreationOperation.Syntax;
+            BaseObjectCreationExpressionSyntax replacementLogEntryCreationExpression;
+
+            var levelAssignment = SyntaxFactory.AssignmentExpression(
+                SyntaxKind.SimpleAssignmentExpression,
+                SyntaxFactory.IdentifierName("Level"),
+                SyntaxFactory.MemberAccessExpression(
+                    SyntaxKind.SimpleMemberAccessExpression,
+                    SyntaxFactory.IdentifierName("LogLevel"),
+                    SyntaxFactory.IdentifierName(logLevel)));
+
+            if (logEntryCreationOperation.Initializer != null)
+            {
+                replacementLogEntryCreationExpression = logEntryCreationExpression.WithInitializer(
+                    logEntryCreationExpression.Initializer.WithExpressions(
+                        SyntaxFactory.SeparatedList(
+                            logEntryCreationExpression.Initializer.Expressions
+                                .Concat(new[] { levelAssignment }))));
+            }
+            else
+            {
+                replacementLogEntryCreationExpression = logEntryCreationExpression.WithInitializer(
+                    SyntaxFactory.InitializerExpression(SyntaxKind.ObjectInitializerExpression,
+                        SyntaxFactory.SeparatedList<ExpressionSyntax>(new[] { levelAssignment })));
+            }
+
+            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+            root = root.ReplaceNode(logEntryCreationExpression, replacementLogEntryCreationExpression);
+
+            return document.WithSyntaxRoot(root);
+        }
+
+        private static async Task<Document> SetLevelParameter(
+            Document document,
+            IObjectCreationOperation logEntryCreationOperation,
+            string logLevel,
+            CancellationToken cancellationToken)
+        {
+            var logEntryCreationExpression = (BaseObjectCreationExpressionSyntax)logEntryCreationOperation.Syntax;
+
+            var levelArgument = SyntaxFactory.Argument(
+                SyntaxFactory.NameColon("level"),
+                default(SyntaxToken),
+                SyntaxFactory.MemberAccessExpression(
+                    SyntaxKind.SimpleMemberAccessExpression,
+                    SyntaxFactory.IdentifierName("LogLevel"),
+                    SyntaxFactory.IdentifierName(logLevel)));
+
+            var replacementLogEntryCreationExpression =
+                logEntryCreationExpression.WithArgumentList(
+                    SyntaxFactory.ArgumentList(
+                        SyntaxFactory.SeparatedList(
+                            logEntryCreationExpression.ArgumentList.Arguments
+                                .Concat(new[] { levelArgument }))));
+
+            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+            root = root.ReplaceNode(logEntryCreationExpression, replacementLogEntryCreationExpression);
+
+            return document.WithSyntaxRoot(root);
+        }
+
+        private IObjectCreationOperation GetLogEntryCreationOperation(IArgumentOperation logEntryArgument)
+        {
+            if (logEntryArgument.Value is IObjectCreationOperation objectCreation)
+                return objectCreation;
+
+            if (logEntryArgument.Value is ILocalReferenceOperation localReference)
+            {
+                var semanticModel = localReference.SemanticModel;
+                var dataflow = semanticModel.AnalyzeDataFlow(localReference.Syntax);
+
+                return dataflow.DataFlowsIn
+                    .SelectMany(symbol => symbol.DeclaringSyntaxReferences.Select(GetObjectCreationOperation))
+                    .FirstOrDefault(operation => operation != null);
+
+                IObjectCreationOperation GetObjectCreationOperation(SyntaxReference syntaxReference)
+                {
+                    var syntax = syntaxReference.GetSyntax();
+
+                    if (semanticModel.GetOperation(syntax) is IVariableDeclaratorOperation variableDeclaratorOperation
+                        && variableDeclaratorOperation.Initializer is IVariableInitializerOperation variableInitializerOperation)
+                    {
+                        return variableInitializerOperation.Value as IObjectCreationOperation;
+                    }
+
+                    return null;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Logging/RockLib.Logging.Analyzers.Test/NoLogLevelSpecifiedAnalyzerTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using RockLibVerifier = RockLib.Logging.Analyzers.Test.CSharpAnalyzerVerifier<
+    RockLib.Logging.Analyzers.NoLogLevelSpecifiedAnalyzer>;
+
+namespace RockLib.Logging.Analyzers.Test
+{
+    [TestClass]
+    public class NoLogLevelSpecifiedAnalyzerTests
+    {
+        [TestMethod]
+        public async Task DiagnosticsReported1()
+        {
+            await RockLibVerifier.VerifyAnalyzerAsync(@"
+using RockLib.Logging;
+using System;
+
+public class Test
+{
+    public void Call_Log_With_LogEntry_With_Level_Not_Set(ILogger logger)
+    {
+        logger.Log([|new LogEntry()|]);
+
+        logger.Log([|new LogEntry(""Hello, world!"")|]);
+
+        LogEntry logEntry1 = new LogEntry();
+        logger.Log([|logEntry1|]);
+
+        LogEntry logEntry2 = new LogEntry(""Hello, world!"");
+        logger.Log([|logEntry2|]);
+    }
+}");
+        }
+
+        [TestMethod]
+        public async Task NoDiagnosticsReported1()
+        {
+            await RockLibVerifier.VerifyAnalyzerAsync(@"
+using RockLib.Logging;
+using System;
+
+public class Test
+{
+    public void Call_Log_With_LogEntry_With_Level_Set(ILogger logger)
+    {
+        logger.Log(new LogEntry(""Hello, world!"", LogLevel.Error));
+
+        logger.Log(new LogEntry { Level = LogLevel.Error });
+
+        LogEntry logEntry1 = new LogEntry(""Hello, world!"", LogLevel.Error);
+        logger.Log(logEntry1);
+
+        LogEntry logEntry2 = new LogEntry { Level = LogLevel.Error };
+        logger.Log(logEntry2);
+
+        LogEntry logEntry3 = new LogEntry();
+        logEntry3.Level = LogLevel.Error;
+        logger.Log(logEntry3);
+    }
+}");
+        }
+    }
+}
