c8bf007bb3f69d8152723300cd5a4c9f63b9baaa
Justin Avery <JustinAvery@QuickenLoans.com>
2021-07-20 08:46:13 -0400

    update diagnostic id

--- Common/DiagnosticIds.cs
+++ Common/DiagnosticIds.cs
@@ -1,10 +1,11 @@
 ﻿namespace RockLib.Analyzers.Common
 {
-    internal static class DiagnosticIds
-    {
-        public const string ExtendedPropertyNotMarkedSafeToLog = "RockLib0000";
-        public const string UseSanitizingLoggingMethod = "RockLib0001";
-        public const string LoggerShouldBeSynchronous = "RockLib0002";
-        public const string RockLibLoggerProviderHasMissingLogger = "RockLib0003";
-    }
+	internal static class DiagnosticIds
+	{
+		public const string ExtendedPropertyNotMarkedSafeToLog = "RockLib0000";
+		public const string UseSanitizingLoggingMethod = "RockLib0001";
+		public const string LoggerShouldBeSynchronous = "RockLib0002";
+		public const string RockLibLoggerProviderHasMissingLogger = "RockLib0003";
+		public const string UseAnonymousObject = "RockLib0005";
+	 }
 }
